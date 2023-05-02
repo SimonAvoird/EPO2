b12137040e8c95edf0068326fb0831232f0337a1
Simon van der Avoird <simon@becanus.nl>
2023-05-02 11:31:57 +0200

    Adding comments

--- main.c
+++ main.c
@@ -2,16 +2,16 @@
 #include<stdlib.h>
 #include<string.h>
 
-struct Station {
+struct Station {                                                                        //the structure for the station coordinates
     int row,column;
 };
-struct Cross {
+struct Cross {                                                                          //the structure for the final route
     char cross;
     int row;
     int column;
 };
 struct Cross route[25];
-int maze[13][13] = {
+int maze[13][13] = {                                                                    //defining the maze
     {-1, -1, -1, -1, 0, -1, 0, -1, 0, -1, -1, -1, -1},
     {-1, -1, -1, -1, 0, -1, 0, -1, 0, -1, -1, -1, -1},
     {-1, -1,  0,  0, 0,  0, 0,  0, 0,  0,  0, -1, -1},
@@ -28,7 +28,7 @@ int maze[13][13] = {
 };
 int cross_in[81];
 char cross_dir[40];
-int field_define(int stop)
+int field_define(int stop)                                                              //function to put the mines in the array
 {
     int i,j,l;
     char k;
@@ -53,7 +53,7 @@ int field_define(int stop)
     return 0;
 }
 
-int route_finder(struct Station begin, struct Station end)
+int route_finder(struct Station begin, struct Station end)                              //this maps all the possible routes, by increasing every number next to the start, and then the next number
 {
     int l,i,j, index = 0;
     i = end.row;
@@ -92,7 +92,7 @@ int route_finder(struct Station begin, struct Station end)
     return 0;
 }
 
-struct Station stations(int station) // this converts the station number from the input to an array of coordinates in the matrix
+struct Station stations(int station)                                                    // this converts the station number from the input to an array of coordinates in the matrix
 {
     struct Station coords;
     switch(station)
@@ -116,22 +116,22 @@ struct Station stations(int station) // this converts the station number from th
     }
     return coords;
 }  
-int route_define(struct Station begin, struct Station end)
+int route_define(struct Station begin, struct Station end)                              //function that maps the crossroads in the chosen route into an array
 {
     int index = maze[begin.row][begin.column];
     int i = begin.row;
     int j = begin.column;
     int amount = 0;
-    while(index > 0)
+    while(index > 0)                                                                    //go from the highest value at the beginning, down to the end where the index is low
     {
         if((i % 2 == 0) && (j % 2 == 0) && (i != 0) && (i != 12) && (j != 0) && (j != 12))
         {
-            route[amount].cross = 'c';
+            route[amount].cross = 'c';                                                  //if a crossroad is passed it should be placed in the array
             route[amount].row = (i - 2)/2;
             route[amount].column = (j-2)/2;
             amount++; 
         }
-        if(maze[i+1][j] == index - 1)
+        if(maze[i+1][j] == index - 1)                                                   //find in which place is the lower value to go to next
         {   
             i++; 
         }
@@ -147,12 +147,12 @@ int route_define(struct Station begin, struct Station end)
         {   
             j--; 
         }
-        index--;
+        index--;                                                                        //lower index value, so it goes to the next maze cell
     }
-    route[amount+1].cross = 'e';
+    route[amount].cross = 'e';                                                          //end of input in the array
     return 0;
 }
-int print_maze(void)
+int print_maze(void)                                                                    //function to print the whole 2d-maze array
 {
     for(int i = 0; i < 13; i++) {
         for(int j = 0; j < 13; j++) {
@@ -172,21 +172,21 @@ int print_maze(void)
 int main(void)
 {
     int k = 0, i = 0,stop, begin_station, end_station;
-    print_maze();
-    scanf("%d", &cross_in[0]);
-    stop = cross_in[0]*2 + 1;
+    print_maze();                                                                       //print the initial maze
+    scanf("%d", &cross_in[0]);                                                          //scan the amount of mines
+    stop = cross_in[0]*2 + 1;                                                           //calculate the needed amount of values in the location array
     for(k = 1; k < stop; k = k + 2)
     {
-        scanf("%d %d %c", &cross_in[k], &cross_in[k+1], &cross_dir[(k-1)/2]);       
+        scanf("%d %d %c", &cross_in[k], &cross_in[k+1], &cross_dir[(k-1)/2]);           //scan for the locations of the mines
     }
-    scanf("%d %d", &begin_station, &end_station);
-    field_define(stop);
+    scanf("%d %d", &begin_station, &end_station);                                       //scan for begin and end
+    field_define(stop);                                                                 //put the mines in the 2d-maze array
     struct Station begin = stations(begin_station);
-    struct Station end = stations(end_station);
-    route_finder(begin, end);
-    print_maze();
-    route_define(begin, end);
-    while(route[i].cross != 'e')
+    struct Station end = stations(end_station);                                         //change the station values into station coordinates
+    route_finder(begin, end);                                                           //map the possible routes
+    print_maze();                                                                       //print the maze with the mines and the possible routes
+    route_define(begin, end);                                                           //choose a route and put the crossroads in an array
+    while(route[i].cross != 'e')                                                        //print the crossroad array
     {
         printf("%c%d%d ", route[i].cross, route[i].row, route[i].column);
         i++;
