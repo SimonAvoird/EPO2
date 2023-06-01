aa1d44f96a09f83e634051faff3eac1057a767de
Simon van der Avoird <simon@becanus.nl>
2023-06-01 16:36:01 +0200

    No real changes

--- serial.c
+++ serial.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <Windows.h>
 #include <string.h>
+#include <wchar.h>
 
 #define COMPORT "COM5"
 #define BAUDRATE CBR_9600
@@ -55,7 +56,6 @@ void initSio(HANDLE hSerial){
 int readByte(HANDLE hSerial, char *buffRead) {
 
     DWORD dwBytesRead = 0;
-
     if (!ReadFile(hSerial, buffRead, 1, &dwBytesRead, NULL))
     {
         printf("error reading byte from input buffer \n");
@@ -72,7 +72,7 @@ int readByte(HANDLE hSerial, char *buffRead) {
 int writeByte(HANDLE hSerial, char *buffWrite){
 
     DWORD dwBytesWritten = 0;
-
+    
     if (!WriteFile(hSerial, buffWrite, 1, &dwBytesWritten, NULL))
     {
         printf("error writing byte to output buffer \n");
@@ -82,12 +82,12 @@ int writeByte(HANDLE hSerial, char *buffWrite){
     return(0);
 }
 
-int main()
+int def_main()
 {
     HANDLE hSerial;
 
 
-    char byteBuffer[BUFSIZ+1];
+    char byteBuffer[BUFSIZ+1] = {0};
 
     //----------------------------------------------------------
     // Open COMPORT for reading and writing
@@ -117,6 +117,7 @@ int main()
     initSio(hSerial);
 
     while ( 1 ) {
+        
         gets(byteBuffer);
 
         if (byteBuffer[0] == 'q') // end the loop by typing 'q'
@@ -125,6 +126,88 @@ int main()
         writeByte(hSerial, byteBuffer);
         readByte(hSerial, byteBuffer);
     }
+
+    printf("ZIGBEE IO DONE!\n");
+    return 0;
+
+    CloseHandle(hSerial);
+}
+
+char convert_array (const int array[8])
+{
+  char result = 0;
+  for(size_t i=0; i<8; i++)
+  {
+    result |= array[i] << (7-i);
+  }
+  return result;
+}
+
+int main()
+{
+    HANDLE hSerial;
+    char values[][8] =
+    {
+        {0,1,1,0,1,1,1,1},
+        {1,1,1,1,1,1,1,1},
+        {0,0,0,0,0,0,0,0}
+    };
+    char results[5][8];
+
+    char byteBuffer[BUFSIZ+1] = {0};
+
+    //----------------------------------------------------------
+    // Open COMPORT for reading and writing
+    //----------------------------------------------------------
+    hSerial = CreateFile(COMPORT,
+        GENERIC_READ | GENERIC_WRITE,
+        0,
+        0,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        0
+    );
+
+    if(hSerial == INVALID_HANDLE_VALUE){
+        if(GetLastError()== ERROR_FILE_NOT_FOUND){
+            //serial port does not exist. Inform user.
+            printf(" serial port does not exist \n");
+        }
+        //some other error occurred. Inform user.
+        printf(" some other error occured. Inform user.\n");
+    }
+
+    //----------------------------------------------------------
+    // Initialize the parameters of the COM port
+    //----------------------------------------------------------
+
+    initSio(hSerial);
+    for(size_t i=0; i<8; i++)
+    {
+        byteBuffer[0] |= values[0][i] << (7-i);
+    }
+    writeByte(hSerial, byteBuffer);
+    Sleep(3000);
+    readByte(hSerial, byteBuffer);
+    atoi(itoa(byteBuffer[0], results[0], 2));
+
+for(size_t i=0; i<8; i++)
+    {
+        byteBuffer[0] |= values[1][i] << (7-i);
+    }
+    writeByte(hSerial, byteBuffer);
+    Sleep(3000);
+    readByte(hSerial, byteBuffer);
+    atoi(itoa(byteBuffer[0], results[1], 2));
+
+for(size_t i=0; i<8; i++)
+    {
+        byteBuffer[0] |= values[2][i] << (7-i);
+    }
+    writeByte(hSerial, byteBuffer);
+    Sleep(3000);
+    readByte(hSerial, byteBuffer);
+    atoi(itoa(byteBuffer[0], results[2], 2));
 
     printf("ZIGBEE IO DONE!\n");
     return 0;
